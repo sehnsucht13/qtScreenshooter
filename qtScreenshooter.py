379c76b7c11d253a32c3cb9e9bb0cec1cac2b17f
Yavor <ykonstantinov1@gmail.com>
2018-12-12 21:58:04 -0800

    Implemented the saving of screenshots. Changed UI and .py file from checkboxes to radiobuttons.

--- qtScreenshooter.py
+++ qtScreenshooter.py
@@ -1,9 +1,10 @@
 import sys
-from PyQt5.QtCore import QTimer 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QDialog, QFileDialog, QShortcut
-from PyQt5.QtGui import QScreen, QPixmap, QKeySequence 
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QDialog, QFileDialog, QShortcut, QRubberBand, QErrorMessage
+from PyQt5.QtGui import QScreen, QPixmap, QKeySequence
 from qtScreenshooterMainWindow import Ui_qtScreenshooterMainWindow
 from qtScreenshooterSaveWindow import Ui_qtScreenshooterSaveWindow
+
 
 class MainWin(QMainWindow, Ui_qtScreenshooterMainWindow):
     def __init__(self):
@@ -45,6 +46,9 @@ class MainWin(QMainWindow, Ui_qtScreenshooterMainWindow):
             self.hide()
             # Delay for 300 milliseconds and take screenshot
             self.windowTimer.singleShot(500, self.takeScreenshot)
+        elif self.capSelectRgnRadBtn.isChecked():
+            band = QRubberBand(QRubberBand.Rectangle)
+            band.setVisible(True)
 
     def takeScreenshot(self, xStart=0, yStart=0, xWidth=0, yHeight=0):
         screen = QApplication.primaryScreen()
@@ -52,31 +56,43 @@ class MainWin(QMainWindow, Ui_qtScreenshooterMainWindow):
         self.saveWindow.showMediaPreview(screenshot)
         self.saveWindow.showWin()
 
+
 class SaveWin(QDialog, Ui_qtScreenshooterSaveWindow):
     def __init__(self):
         QDialog.__init__(self)
         Ui_qtScreenshooterSaveWindow.__init__(self)
         self.setupUi(self)
         self.cancelBtn.clicked.connect(self.saveWinCancelBtn)
+        self.acceptBtn.clicked.connect(self.acceptBtnHandler)
 
     def saveWinCancelBtn(self):
-        self.close()    
+        self.close()
 
     def showWin(self):
         self.show()
 
     def showMediaPreview(self, media):
-        image = QPixmap(media)
-        self.mediaPreviewLabel.setPixmap(image.scaled(300, 230))
-        ff = QFileDialog(self, "Save", "/home")
-        ff.exec()
+        self.image = QPixmap(media)
+        self.mediaPreviewLabel.setPixmap(self.image.scaled(361, 230))
 
+    def acceptBtnHandler(self):
+        if(self.saveCheckbox.isChecked()):
+            fileName =  QFileDialog.getSaveFileName(self, "Save File", "", "Images (*.jpg, *.png)")
+            if(fileName[0] != ""):
+                if(fileName[0].endswith(".jpg")):
+                    self.image.save(fileName[0], "jpg")
+                else:
+                    self.image.save(fileName[0].join(".jpg"), "jpg")
+            else:
+                errorMsg = QErrorMessage.showMessage("Please enter a name for the file!")
+            
 
 def main():
     app = QApplication(sys.argv)
-    mainWindow = MainWin() 
+    mainWindow = MainWin()
     mainWindow.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
